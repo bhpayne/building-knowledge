5d1b87976cc713fcc1c779bef58146fef9c19b82
Ben Payne <ben.is.located@gmail.com>
2015-06-30 21:23:21 -0400

    working

---
+++ peak_power/use_all_memory.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    while(1)
+    {
+           void *m = malloc(1024*1024*1024);
+           memset(m,0,1024*1024*1024);
+           memset(m,1,1024*1024*1024);
+           free(m);
+    }
+    return 0;
+}
+
+
