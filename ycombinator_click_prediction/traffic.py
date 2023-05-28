df028fcee76850e85454aaa3a17934040469dae2
Ben Payne <ben.is.located@gmail.com>
2023-05-28 18:19:29 -0400

    moved to other repos

--- ycombinator_click_prediction/traffic.py
+++
@@ -1,6 +0,0 @@
-from HN.models import ycombinator
-import geocoder,re 
-data=[x[0] for x in ycombinator.objects.values_list('ip_address')]
-unique_ips=set(data)
-traffic_stats=[(x,re.sub(',\sUS.*','',str(geocoder.ip(x)).split('[')[-1]),data.count(x)) for x in unique_ips]
-results=sorted(traffic_stats,key=lambda x: -x[2])
