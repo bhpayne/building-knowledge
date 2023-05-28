1b17d3caab75aac35b305f7f26d7e3f8652b62ca
Ben Payne <ben.is.located@gmail.com>
2023-05-28 17:50:49 -0400

    more descriptive

--- parallel_bash/parallel.sh
+++ parallel_bash/parallel.sh
@@ -1,10 +1,6 @@
 # subshells
 
-# serial:
-#sleep 2; echo "hello"
-#sleep 1; echo "I'm ben"
-#sleep 3; echo "mike"
 
 ( sleep 2; echo "hello" ) &
-( sleep 1; echo "I'm ben" ) &
-( sleep 3; echo "mike" ) &
+( sleep 1; echo "second" ) &
+( sleep 3; echo "third" ) &
---
+++ parallel_bash/serial.sh
@@ -0,0 +1,6 @@
+
+# serial:
+sleep 2; echo "hello"
+sleep 1; echo "second"
+sleep 3; echo "third"
+
